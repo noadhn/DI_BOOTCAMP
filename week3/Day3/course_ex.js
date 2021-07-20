88d80db539420eb76e2a7df8bd935d66264f370a
noadhn <noasworking@gmail.com>
2021-07-20 21:27:51 +0300

    finally add ex only

---
+++ week3/Day3/course_ex.js
@@ -0,0 +1,26 @@
+
+// let age= prompt("What's your age miss?");
+// age= Number(age);
+// if (age < 18){
+//   alert("Sorry, you are too young to drive this car. Powering off");
+// }
+// else if (age === 18){
+//   alert("Congratulations on your first year of driving. Enjoy the ride!");
+// }
+// else {
+//   alert("Powering On. Enjoy the ride!");
+// }
+// Create a stuctured html file linked to a JS file
+//
+
+let user= {
+  username: "noadhn",
+  password: "Noadahan",
+};
+let newsfeedObject= {
+  username: "",
+  timeline: "",
+};
+let database= [user];
+let newsfeed=[newsfeedObject, newsfeedObject, newsfeedObject];
+console.log(newsfeed);
