9141296663ae03525d7e21d4eb9d64d4a07bddc3
noadhn <noasworking@gmail.com>
2021-12-19 19:48:30 +0200

    add all bootcamp project except HACAKATON1-mp3-player

---
+++ kaora/kaora/static/js.js
@@ -0,0 +1,5 @@
+$(function(){
+        $(".navigation .nav_bar .logo-toggle-container .toggle-box").click(function(){
+            $(".navigation .nav_bar .menu").stop().slideToggle()
+        });
+    })
