47abeef061716cc87f05657890985f76c3742a35
noadhn <noasworking@gmail.com>
2021-12-28 19:13:06 +0200

    first commit

--- kaora/kaora/static/js.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-        $(".navigation .nav_bar .logo-toggle-container .toggle-box").click(function(){
-            $(".navigation .nav_bar .menu").stop().slideToggle()
-        });
-    })
--- kaora/run.py
+++
@@ -1,4 +0,0 @@
-from kaora import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
